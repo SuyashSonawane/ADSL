9d3bebf2e20dabe07c07fd29b77c081577bf0f9a
Admin <suyashsonawane>
2020-01-10 10:56:35 +0530

    tbt

--- TBT/main.cpp
+++ TBT/main.cpp
@@ -10,78 +10,123 @@ struct node
 
 };
 
-class TBT{
-	node *root , *dummy;
+class TBT
+{
+	node *dummy,*root;
 
-	node *TBT_insert(node *root,int data,node*parent)
+	void BST_print_in(node *root)
 	{
-		if(root==NULL)
+
+		if(root==dummy)
 		{
-			node *nn = new node;
-			nn->data=data;
-			nn->left=nn->right=parent;
-			nn->isr=nn->isl=true;
-			root = nn;
-			return root;
+			return;
 		}
-		if(root->data<parent->data)
-		{
-			parent->isl=false;
-			parent->left = TBT_insert(root->)
-		}
+				print_node(root);
+
+		BST_print_in(root->left);
+		BST_print_in(root->right);
 
 	}
-
-
-
+	void print_node(node *n)
+	{
+		cout<<"\nData  : "<<n->data;
+		cout<<"\n";
+	}
 public:
 	TBT()
 	{
 		root=NULL;
-		dummy = new node;
-		dummy->isr=dummy->isl=false;
 		dummy->left=root;
-		dummy->right = NULL;
+		dummy->right=dummy;
 
 	}
-	void insert()
+	void insert(int k)
 	{
-		cout<<"Enter data : ";
-		cin>>data;
-		node *nn = new node;
-		nn->data=data;
-		nn->left=nn->right=dummy;
-		nn->isr=nn->isl=true;
-
-		node *temp=root;
-		
-		while(1)
+		if(root==NULL)
 		{
-			if(root==NULL)
+			node *nn = new node;
+			nn->data=k;
+			nn->left=dummy;
+			nn->right=dummy;
+			nn->isr=nn->isl=true;
+			root=nn;
+			return;
+		}
+		else
+		{
+			node *temp=root;
+			node *parent;
+			do
 			{
-				root=nn;
-				break;
-			}
-			else
-				if(nn->data < temp->data)
+				if(k<temp->data)
 				{
-					if(temp->isl==false)
-					{
-						temp=temp->left;
-						continue;
-					}
-
-
+					parent=temp;
+					temp=temp->left;
+					continue;
 				}
+				if(k>temp->data)
+				{
+					parent=temp;
+					temp=temp->right;
+					continue;
+				}
+			}
+			while(temp!=dummy);
+			if(parent->isr==true && parent->isl==true)
+			{
+				node *nn = new node;
+				nn->data=k;
+				nn->left=parent->left;
+				nn->right=parent->right;
+				nn->isr=nn->isl=true;
+				if(k>parent->data)
+				{
+					parent->right=nn;
+					parent->isr=false;
+				}
+				else
+				{
+					parent->left=nn;
+					parent->isl=false;
+				}
+			}
+			if(parent->isr==true && parent->isl==false)
+			{
+					node *nn = new node;
+					nn->data=k;
+					nn->left=parent->left;
+					nn->right=parent->right;
+					nn->isr=nn->isl=true;
+					parent->right=nn;
+					parent->isr=false;
+			}
+			if(parent->isr==false && parent->isl==true)
+			{
+					node *nn = new node;
+					nn->data=k;
+					nn->left=parent->left;
+					nn->right=parent->right;
+					nn->isr=nn->isl=true;
+					parent->left=nn;
+					parent->isl=false;
+			}
 		}
 	}
+	void print()
+	{
+		BST_print_in(root);
+		cout<<"\n";
+	}
+	
 
 };
 
-
-
 int main()
 {
+	TBT a;
+	a.insert(10);
+	a.insert(5);
+	cout<<"Inserted\n";
+	a.print();
 
 }
-
